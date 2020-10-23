93f1781ffd0ad4b67ccbc86f28020c6a5e765078
nickryz <nickryz87@gmail.com>
2020-10-23 17:29:21 +0300

    some fixes

--- resources/views/obvikids.blade.php
+++ resources/views/obvikids.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Obvi Kids | Wealth M.jr</title>
+    <title>Obvi\`s Kid Complete Protein</title>
 
     <!-- Font Awesome -->
     <link rel="stylesheet" href="/obvikids/css/font-awesome.min.css">
